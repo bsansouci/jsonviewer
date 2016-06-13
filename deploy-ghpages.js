dd3188dc2f5240b0e9bc00576bb147c57ec2a76d
Benjamin San Souci <benjamin.sansouci@gmail.com>
2016-06-13 14:53:50 -0700

    Deploy

--- deploy-ghpages.js
+++ deploy-ghpages.js
@@ -1,2 +1,14 @@
 var ghpages = require('gh-pages');
+var fs = require('fs');
+var path = require('path');
+
+function copyFolder( source, target ) {
+  files = fs.readdirSync( source );
+  files.forEach( function ( file ) {
+    fs.createReadStream(path.join( source, file ))
+      .pipe( fs.createWriteStream(path.join( target, file )) );
+  } );
+}
+
+copyFolder('assets','dist');
 ghpages.publish('./dist', console.error.bind(console));
